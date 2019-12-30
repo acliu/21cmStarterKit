a348bd32240b93f00c95886991f46ef23f6aa4d4
joellebegin <joellebegin.6@gmail.com>
2019-12-30 18:17:48 -0500

    adding a little bash script

---
+++ bash_scrips/distributions.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+def main():
+    '''generates a bunch of .txt files with different distributions'''
+    poisson = np.random.poisson(size = 2000)
+    gaussian = np.random.normal(size = 2000)
+    pareto = np.random.pareto(3, size = 2000)
+
+    np.savetxt('poisson_distribution_2000_datapoints.txt', poisson, delimiter=',')
+    np.savetxt('gaussian_distribution_2000_datapoints.txt', gaussian, delimiter=',')
+    np.savetxt('pareto_distribution_2000_datapoints.txt', pareto, delimiter=',')
+
+if __name__ == "__main__":
+    main()
---
+++ bash_scrips/histograms.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+def main():
+    file_loc = input()
+    data = np.loadtxt(file_loc, delimiter = ',')
+
+    fig, ax = plt.subplots()
+    ax.hist(data, density = True)
+
+    plt.savefig(file_loc + '.png')
+
+if __name__ == "__main__":
+    main()
