c313abfccc62216fdd19e73d0807238b045af3d3
joellebegin <joellebegin.6@gmail.com>
2020-06-24 21:02:00 -0400

    more comments on the sample script!

--- Parallel_Computing/my_first_parallel.py
+++ Parallel_Computing/my_first_parallel.py
@@ -1,11 +1,16 @@
 import numpy as np
 from mpi4py import MPI
 
-"""This code squares every number in an array using MPI parallel processing. 
+"""
+This code squares every number in an array using MPI parallel processing. 
 You run code with many processors with the syntax (on your shell):
     $ mpiexec -n 4 python script.py
 where the number after -n is the number of processors you want to run the 
-script with. """
+script with. 
+
+By Joelle Begin, modified from Adrian Liu's code and extensively commented. 
+June 2020
+"""
 
 #this just talks to your computer to see how many processors are available
 comm = MPI.COMM_WORLD
@@ -19,7 +24,7 @@ master = 0
 # -1 since one of the processors is master
 numHelpers = comm.size -1
 
-'''the array we want to perform the operation on'''
+#the array we want to perform the operation on
 array = np.arange(1,10,1 )
 
 '''we have to square every number in the array, so there are as many tasks
@@ -31,21 +36,25 @@ as there are tasks to be active'''
 num_active_helpers = min(numHelpers, num_tasks)
 
 ''' Every CPU sees the same script; we have to have conditional statements 
-so the CPUs know what tasks to perform. '''
+so the CPUs know what tasks to perform based on their ID which was assigned in
+line 19. '''
 
 if myID == master:
     '''master will manage the parallelization. It sends out tasks, recieves
     the results of the operation and gathers everything into a result array'''
     result = np.zeros(len(array)) #construcing empty array to hold results
 
-    num_sent = 0
+    num_sent = 0 #need to keep track of how many assignments have been sent out!
+    
     print('sending out assignments')
     for helperID in range(1, num_active_helpers+1):
+        #looping through the helpers; these are the initial assignments
         print('I asked', helperID, 'to do number', helperID)
         
-        '''this sends the value helperID -1 to processor with ID == helperID.
+        '''this sends the value (helperID -1) to processor with ID == helperID.
         The tag is going to help us keep track of whether the process is finished'''
         comm.send(helperID-1, dest = helperID, tag = helperID)
+        
         num_sent += 1
 
     for i in range(1, num_tasks+1):
@@ -67,7 +76,7 @@ if myID == master:
         if num_sent < num_tasks: #ie there are more things to do
             '''sends next task to the processor who just sent results; we know
             this processor doesn't have anything to do since it just finished
-            its previous task'''
+            its previous task!'''
             comm.send(num_sent, dest = sender, tag = 1)
             print('I asked', sender, 'to do number', num_sent +1)
             num_sent += 1
@@ -96,9 +105,18 @@ elif myID <= num_active_helpers:
         if tag ==0:
             complete = True
         else:
+            #getting the number it has to perform the operation on
             num = array[assignment]
-            comm.send(num**2, dest = master, tag = assignment) #sending master result
 
+            #performing the operation
+            num_squared = num**2
 
-#code past this barrier will not be executed until the above processes are done
+            #sending the result back to master
+            comm.send(num_squared, dest = master, tag = assignment)
+
+'''code past this barrier will not be executed until the above processes are done
+This is good to have in case you need to do more stuff to the result of the 
+parallelization!
+'''
 comm.Barrier() 
+
